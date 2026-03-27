a854a8874ed847405b568412c1d27bc9f400a5ea
Diego Magnata <dfm@cesar.school>
2026-03-27 19:38:33 -0300

    feat: Add Contact entity to entidades package
    IMPLEMENTATION:
    - Create Contact class in br.edu.cs.poo.ac.bolsa.entidades
    - Define attributes: email, telefoneFixo, telefoneCelular, numeroWhatsApp, nomeParaContato
    - Implement full constructor with parameter initialization
    - Add accessor methods (getters/setters) for all fields
    - Enable flexible contact management with multiple channels

---
+++ src/br/edu/cs/poo/ac/bolsa/entidades/Contatos.java
@@ -0,0 +1,57 @@
+package br.edu.cs.poo.ac.bolsa.entidades;
+
+public class Contatos {
+	private String email;
+	private String telefoneFixo;
+	private String telefoneCelular;
+	private String numeroWhatszap;
+	private String nomeParaContato;
+	
+	public Contatos(String email, String telefoneFixo, String telefoneCelular, String numeroWhatszap, String nomeParaContato) {
+		this.email = email;
+		this.telefoneFixo = telefoneFixo;
+		this.telefoneCelular = telefoneCelular;
+		this.numeroWhatszap = numeroWhatszap;
+		this.nomeParaContato = nomeParaContato;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefoneFixo() {
+		return telefoneFixo;
+	}
+
+	public void setTelefoneFixo(String telefoneFixo) {
+		this.telefoneFixo = telefoneFixo;
+	}
+
+	public String getTelefoneCelular() {
+		return telefoneCelular;
+	}
+
+	public void setTelefoneCelular(String telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+
+	public String getNumeroWhatszap() {
+		return numeroWhatszap;
+	}
+
+	public void setNumeroWhatszap(String numeroWhatszap) {
+		this.numeroWhatszap = numeroWhatszap;
+	}
+
+	public String getNomeParaContato() {
+		return nomeParaContato;
+	}
+
+	public void setNomeParaContato(String nomeParaContato) {
+		this.nomeParaContato = nomeParaContato;
+	}
+}
