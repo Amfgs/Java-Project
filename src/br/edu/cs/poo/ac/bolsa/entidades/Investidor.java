b7faf06c8d06711c124171f49d151e0376fccef1
Diego Magnata <dfm@cesar.school>
2026-03-27 19:39:23 -0300

    feat: Add Investor base entity to entidades package
    IMPLEMENTATION:
    - Create Investor class in br.edu.cs.poo.ac.bolsa.entidades
    - Define attributes: nome, endereco, dataCriacao, bonus, contatos
    - Implement full constructor with all parameter initialization
    - Add public accessor methods for public attributes
    - Add protected accessor methods for creation date (for subclass inheritance)
    - Implement getAge() using Period.between() for age calculation
    - Implement creditarBonus() and debitarBonus() with BigDecimal operations
    - Support inheritance model for specialized investor types

---
+++ src/br/edu/cs/poo/ac/bolsa/entidades/Investidor.java
@@ -0,0 +1,72 @@
+package br.edu.cs.poo.ac.bolsa.entidades;
+
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import java.time.Period;
+
+public class Investidor {
+	private String nome;
+	private Endereco endereco;
+	private LocalDate dataCriacao;
+	private BigDecimal bonus;
+	private Contatos contatos;
+	
+	public Investidor(String nome, Endereco endereco, LocalDate dataCriacao, BigDecimal bonus, Contatos contatos) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.dataCriacao = dataCriacao;
+		this.bonus = bonus;
+		this.contatos = contatos;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Endereco getEndereco() {
+		return this.endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	public Contatos getContatos() {
+		return this.contatos;
+	}
+	
+	public void setContatos(Contatos contatos) {
+		this.contatos = contatos;
+	}
+	
+	protected LocalDate getDataCriacao() {
+		return this.dataCriacao;
+	}
+	
+	protected void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	public BigDecimal getBonus() {
+		return this.bonus;
+	}
+	
+	public int getIdade() {
+		LocalDate atual = LocalDate.now();	
+		Period periodo = Period.between(this.dataCriacao, atual);
+		int idade = periodo.getYears();
+		return idade;
+	}
+	
+	public void creditarBonus(BigDecimal valor) {
+		this.bonus = this.bonus.add(valor);
+	}
+	
+	public void debitarBonus(BigDecimal valor) {
+		this.bonus = this.bonus.subtract(valor);
+	}
+}
